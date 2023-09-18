3d0a5d510007f6f36bc7fe3307455501bc6663dd
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-18 15:40:08 +0200

    :sparkle: Interface for get service

--- src/services/JobAdSearchServices.ts
+++ src/services/JobAdSearchServices.ts
@@ -1,3 +1,4 @@
+import { IGetJobAdsResponse } from '@/models/IGetJobAdsResponse';
 import { get } from './ServiceBase';
 
 // Base values that pre-filters search results
@@ -11,22 +12,24 @@ interface IGetJobAds {
 
 export const getJobAds = async (params: IGetJobAds) => {
 	if ((params.historicalFrom && params.historicalTo) === undefined) {
-		return await get(`search?employer=${params.employer}${REQUEST_SPECCS}`);
+		return await get<IGetJobAdsResponse>(
+			`search?employer=${params.employer}${REQUEST_SPECCS}`
+		);
 	}
 
 	if (params.historicalTo === undefined) {
-		return await get(
+		return await get<IGetJobAdsResponse>(
 			`search?employer=${params.employer}&historical-from=${params.historicalFrom}${REQUEST_SPECCS}`
 		);
 	}
 
 	if (params.historicalFrom === undefined) {
-		return await get(
+		return await get<IGetJobAdsResponse>(
 			`search?employer=${params.employer}&historical-to=${params.historicalTo}${REQUEST_SPECCS}`
 		);
 	}
 
-	return await get(
+	return await get<IGetJobAdsResponse>(
 		`search?employer=${params.employer}&historical-from=${params.historicalFrom}&historical-to=${params.historicalTo}${REQUEST_SPECCS}`
 	);
 };
