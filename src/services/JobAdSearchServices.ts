a669f65265ea922c0977b2097669ae94a9abd40d
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-18 12:10:48 +0200

    :sparkle: add serviceBase and getJobAds call.

---
+++ src/services/JobAdSearchServices.ts
@@ -0,0 +1,32 @@
+import { get } from './ServiceBase';
+
+// Base values that pre-filters search results
+const REQUEST_SPECCS = import.meta.env.REQUEST_SPECCS;
+
+interface IGetJobAds {
+	employer: string;
+	historicalFrom?: string;
+	historicalTo?: string;
+}
+
+export const getJobAds = async (params: IGetJobAds) => {
+	if ((params.historicalFrom && params.historicalTo) === undefined) {
+		return await get(`search?employer=${params.employer}${REQUEST_SPECCS}`);
+	}
+
+	if (params.historicalTo === undefined) {
+		return await get(
+			`search?employer=${params.employer}&historical-from=${params.historicalFrom}${REQUEST_SPECCS}`
+		);
+	}
+
+	if (params.historicalFrom === undefined) {
+		return await get(
+			`search?employer=${params.employer}&historical-to=${params.historicalTo}${REQUEST_SPECCS}`
+		);
+	}
+
+	return await get(
+		`search?employer=${params.employer}&historical-from=${params.historicalFrom}&historical-to=${params.historicalTo}${REQUEST_SPECCS}`
+	);
+};
