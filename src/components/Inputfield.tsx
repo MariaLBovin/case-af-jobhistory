50d15f3853a1012bebd6210c3211810d7e0a39c1
MariaLBovin <maria.bovin@gmail.com>
2023-09-19 11:10:12 +0200

    sending value to parent

--- src/components/Inputfield.tsx
+++ src/components/Inputfield.tsx
@@ -1,70 +1,72 @@
 
-import { ButtonType, ButtonVariation, FormInputSearchVariation, FormInputType, FormInputValidation, FormInputVariation } from '@digi/arbetsformedlingen'
-import { DigiButton, DigiFormInput, DigiFormInputSearch } from '@digi/arbetsformedlingen-react'
+import { FormInputType, FormInputValidation, FormInputVariation } from '@digi/arbetsformedlingen'
+import { DigiButton, DigiFormInput, DigiFormValidationMessage } from '@digi/arbetsformedlingen-react'
 import searchStyles from '../styles/search.module.css'
 import { FieldValues, useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { DigiFormInputCustomEvent, } from '@digi/arbetsformedlingen/dist/types/components'
 
+interface IInputfieldProps {
+    handleValue: (value: string | number) => void
+}
 
-export const Inputfield = () => {
-    const {register, handleSubmit, formState: {errors}, reset, getValues}=useForm()
+export const Inputfield = ({handleValue} :IInputfieldProps) => {
+    // const {
+    //     register,
+    //     handleSubmit,
+    //     formState: { errors, isSubmitting },
+    //     reset,
+    //   } = useForm();
 
-    const onSubmit = (data :FieldValues) => {
-        console.log(data);
-        reset()
-        
+    const [userInput, setUserInput] = useState<string | number>('')
+
+    const handleChange = (e: DigiFormInputCustomEvent<HTMLInputElement>) => {
+        setUserInput(e.target.value)
     }
+
+    const onSubmit = () => {
+
+        if (userInput !== '') {
+            handleValue(userInput);
+            setUserInput('');
+          } else {
+            console.log('fältet är tomt');
+            
+            FormInputValidation.ERROR
+          }
+        // handleValue(userInput)
+        // setUserInput('')
+    }
+    
 
   return (
     <>
     <div className={searchStyles.search_container}>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    {/* <form onSubmit={handleSubmit(onSubmit)}> */}
     <DigiFormInput
-	afLabel="Etikett"
+	afLabel="Sök efter arbetsgivare med namn eller organisationsnummer"
+    afLabelDescription="T.ex. Arbetsförmedlingen"
+    afValue={userInput}
 	afVariation={FormInputVariation.MEDIUM}
 	afType={FormInputType.TEXT}
-	afValidation={FormInputValidation.NEUTRAL}	
-    // {...register('Arbetsgivare', 
-    // {required: 'Sökord krävs',
-    // minLength: {
-    //     value: 3,
-    //     message: 'Du behöver ange ett sökord'
-    // }
+    onAfOnInput={handleChange}
+    // {...register('Sökord', {
+    //     required: 'Du måste ange ett sökord'
     // })}
-    
->   
-{/* {errors.search && (
-        <p>{`${errors.search.message}`}</p>
-        )}		 */}
-        
-    </DigiFormInput>
-    <DigiButton
+    >
+        {/* {errors.TEXT && (
+        <p className="text-red-500">{`${errors.TEXT.message}`}</p>
+      )} */}
+        <DigiButton slot='button'
         onAfOnClick={onSubmit}
-        afVariation={ButtonVariation.PRIMARY}
-      >
-        Klicka
-      </DigiButton>
-    </form>
-    {/* <DigiFormInputSearch
-	afLabel="Sök arbetsgivare med namn"
-	afVariation={FormInputSearchVariation.MEDIUM}
-	afType={FormInputType.SEARCH}	
-    afLabelDescription="T.ex. Arbetsförmedlingen"	
-	afButtonText="Sök"
-    afButtonType= {ButtonType.SUBMIT}
-    onClick={handleSubmit(onSubmit)}
-    className={searchStyles.searchfield}
-    {...register('Arbetsgivare', 
-    {required: 'Sökord krävs',
-    minLength: {
-        value: 3,
-        message: 'Du behöver ange ett sökord'
-    }
-    })}
-    
->   {errors.search && (
-        <p>{`${errors.search.message}`}</p>
-        )}
-    </DigiFormInputSearch> */}
+        >Sök</DigiButton>
+        {/* <DigiFormValidationMessage
+        afVariation={FormValidationMessageVariation.SUCCESS}
+        >
+
+        </DigiFormValidationMessage> */}
+    </DigiFormInput>    
+    {/* </form> */}
     </div>
     
 </>
