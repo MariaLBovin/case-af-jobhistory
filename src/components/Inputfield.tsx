31e2de3e7017a13f1d408637d25786e3335ced24
MariaLBovin <maria.bovin@gmail.com>
2023-09-19 15:33:34 +0200

    validation fixes

--- src/components/Inputfield.tsx
+++ src/components/Inputfield.tsx
@@ -2,7 +2,6 @@
 import { FormInputType, FormInputVariation, FormValidationMessageVariation } from '@digi/arbetsformedlingen'
 import { DigiButton, DigiFormInput, DigiFormValidationMessage } from '@digi/arbetsformedlingen-react'
 import searchStyles from '../styles/search.module.css'
-import { FieldValues, useForm } from 'react-hook-form'
 import { useState } from 'react'
 import { DigiFormInputCustomEvent, } from '@digi/arbetsformedlingen/dist/types/components'
 
@@ -11,32 +10,25 @@ interface IInputfieldProps {
 }
 
 export const Inputfield = ({handleValue} :IInputfieldProps) => {
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     formState: { errors, isSubmitting },
-    //     reset,
-    //   } = useForm();
 
     const [userInput, setUserInput] = useState<string | number>('')
     const [buttonClicked, setButtonClicked] =useState(false)
+    //console.log(buttonClicked);
 
     const handleChange = (e: DigiFormInputCustomEvent<HTMLInputElement>) => {
         setUserInput(e.target.value)
     }
+ 
 
     const onSubmit = () => {
-        setButtonClicked(true);
-
-        if (userInput !== '') {
-            handleValue(userInput);
-            setUserInput('');
-          
-        // handleValue(userInput)
-        // setUserInput('')
-    }
-}
-    
+        if (userInput === '') {
+          setButtonClicked(true);
+        } else {
+          handleValue(userInput);
+          setUserInput('');
+          setButtonClicked(false);
+        }
+      };
 
   return (
     <>
@@ -49,23 +41,19 @@ export const Inputfield = ({handleValue} :IInputfieldProps) => {
 	afVariation={FormInputVariation.MEDIUM}
 	afType={FormInputType.TEXT}
     onAfOnInput={handleChange}
-    // {...register('Sökord', {
-    //     required: 'Du måste ange ett sökord'
-    // })}
     >
-        {/* {errors.TEXT && (
-        <p className="text-red-500">{`${errors.TEXT.message}`}</p>
-      )} */}
+        
         <DigiButton slot='button'
         onAfOnClick={onSubmit}
         >Sök</DigiButton>
-      
-    </DigiFormInput>    
-    {buttonClicked && userInput === '' ? (
-        <DigiFormValidationMessage afVariation={FormValidationMessageVariation.ERROR}>Fältet får inte vara tomt</DigiFormValidationMessage>
-      ) : null}
+        
+    </DigiFormInput>
+    {buttonClicked && userInput === '' && (
+          <DigiFormValidationMessage afVariation={FormValidationMessageVariation.ERROR}>
+            Fältet får inte vara tomt
+          </DigiFormValidationMessage>
+        )}
     </div>
-    
 </>
   )
 }
