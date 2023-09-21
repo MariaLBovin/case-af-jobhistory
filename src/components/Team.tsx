4b9612576097ae6297e972b7b7d73b4c9f3d76dd
MariaLBovin <maria.bovin@gmail.com>
2023-09-21 14:21:19 +0200

    added teammember-component to team

---
+++ src/components/Team.tsx
@@ -0,0 +1,14 @@
+import { teamMembers } from "../arrays/teammembers"
+import { Members } from "./Members"
+
+export const Team = () => {
+  return (
+    <>
+      <ul>
+        {teamMembers.map((member, index) => (
+          <Members member={member} key={index}></Members>
+        ))}
+      </ul>
+    </>
+  )
+}
