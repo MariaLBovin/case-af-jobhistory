9b2dc06ce69e8df14c70c7c351e94672684b7702
MariaLBovin <maria.bovin@gmail.com>
2023-09-21 15:26:51 +0200

    members added

--- src/arrays/teammembers.ts
+++ src/arrays/teammembers.ts
@@ -1,7 +1,25 @@
 import { TeamMembers } from "../models/TeammemberClass";
 
 export const teamMembers: TeamMembers[] = [
-    new TeamMembers("Joel Del Pilar", "joel.delpilar@medieinstitutet.se", "JoeldelPilar"),
-    new TeamMembers("Björn Sjöstedt", "bjorn.sjostedt@medieinstitutet.se", "etrigo"),
-    new TeamMembers("Maria Larsson Bovin", "maria.larssonbovin@medieinstitutet.se", "MariaLBovin"),
+    new TeamMembers
+    ('Joel Del Pilar', 
+    'joel.delpilar@medieinstitutet.se', 
+    'https://github.com/JoeldelPilar', 
+    '/joeldelpilar.jpg',
+    'https://www.linkedin.com/in/joeldelpilar/' 
+    ),
+    new TeamMembers
+    ('Björn Sjöstedt', 
+    'bjorn.sjostedt@medieinstitutet.se', 
+    'https://github.com/etrigo', 
+    '/bjornsjostedt.jpg',
+    'https://www.linkedin.com/in/bjornsjostedt/'
+    ),
+    new TeamMembers(
+      'Maria Larsson Bovin', 
+      'maria.larssonbovin@medieinstitutet.se', 
+      'https://github.com/MariaLBovin', 
+      '/marialarssonbovin.jpg',
+      'https://www.linkedin.com/in/mariabovin/'
+      ),
   ];
