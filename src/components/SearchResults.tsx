5078cf9513415fa468d3078bb5bccd35574a41c6
MariaLBovin <maria.bovin@gmail.com>
2023-09-21 14:21:06 +0200

    moved adsarray to own file

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,87 +1,12 @@
-import { IAd } from "@/models/IAd";
-import { Link } from "react-router-dom";
+import { IAd } from "../models/IAd";
+
 import listStyle from '../styles/displaySearchReslut.module.css'
 import { FormSelectVariation, FormSelectValidation } from "@digi/arbetsformedlingen";
 import { DigiFormSelect, DigiNavigationPagination } from "@digi/arbetsformedlingen-react";
 import { useState } from "react";
 import { DigiFormSelectCustomEvent } from "@digi/arbetsformedlingen/dist/types/components";
 import { ResultList } from "./ResultList";
-
-const newAd1: IAd = {
-  id: "12345",
-  headline: "Ledig tjänst",
-  application_deadline: "2023-12-31",
-  publication_date: "2023-12-31",
-  number_of_vacancies: 3,
-  description: {
-    text: "Detta är en jobbannons."
-  },
-  employment_type: {
-    label: "Heltid"
-  },
-  salary_description: "Konkurrenskraftig lön",
-  employer: {
-    url: "https://www.arbetsgivare.com",
-    organization_number: "1234567890",
-    name: "Arbetsgivare AB",
-    workplace: "Stockholm"
-  },
-  occupation: {
-    label: "Webbutvecklare"
-  },
-  occupation_group: {
-    label: "IT och telekommunikation"
-  },
-  occupation_field: {
-    label: "Teknik och naturvetenskap"
-  },
-  must_have: {
-    languages: ["JavaScript"],
-    education: ["Universitetsexamen"]
-  },
-  nice_to_have: {
-    languages: ["Python"],
-    education: ["Masterexamen"]
-  }
-};
-
-const newAd2: IAd = {
-  id: "12345",
-  headline: "Ledig tjänst",
-  application_deadline: "2023-12-31",
-  publication_date:"2023-12-31",
-  number_of_vacancies: 3,
-  description: {
-    text: "Detta är en jobbannons."
-  },
-  employment_type: {
-    label: "Heltid"
-  },
-  salary_description: "Konkurrenskraftig lön",
-  employer: {
-    url: "https://www.arbetsgivare.com",
-    organization_number: "1234567890",
-    name: "Arbetsgivare AB",
-    workplace: "Stockholm"
-  },
-  occupation: {
-    label: "Webbutvecklare"
-  },
-  occupation_group: {
-    label: "IT och telekommunikation"
-  },
-  occupation_field: {
-    label: "Teknik och naturvetenskap"
-  },
-  must_have: {
-    languages: ["JavaScript"],
-    education: ["Universitetsexamen"]
-  },
-  nice_to_have: {
-    languages: ["Python"],
-    education: ["Masterexamen"]
-  }
-};
+import { newAd1, newAd2 } from "../arrays/ads";
 
 const ads :IAd[] = [newAd1, newAd2]
 
