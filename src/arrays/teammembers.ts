42c6262560a6baf059e13cc26a162cd66d6cf281
MariaLBovin <maria.bovin@gmail.com>
2023-09-21 14:20:16 +0200

    team members array added

---
+++ src/arrays/teammembers.ts
@@ -0,0 +1,7 @@
+import { TeamMembers } from "../models/TeammemberClass";
+
+export const teamMembers: TeamMembers[] = [
+    new TeamMembers("Joel Del Pilar", "joel.delpilar@medieinstitutet.se", "JoeldelPilar"),
+    new TeamMembers("Björn Sjöstedt", "bjorn.sjostedt@medieinstitutet.se", "etrigo"),
+    new TeamMembers("Maria Larsson Bovin", "maria.larssonbovin@medieinstitutet.se", "MariaLBovin"),
+  ];
