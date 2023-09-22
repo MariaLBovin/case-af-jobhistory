d84119e6c098a2088d2fe237658269f2dabb6d41
MariaLBovin <maria.bovin@gmail.com>
2023-09-22 14:58:02 +0200

    go back button created

---
+++ src/components/GoBackButton.tsx
@@ -0,0 +1,31 @@
+import { ButtonSize, ButtonVariation } from "@digi/arbetsformedlingen"
+import { DigiButton, DigiIcon, } from "@digi/arbetsformedlingen-react"
+import { useNavigate } from "react-router-dom"
+import buttonStyles from '../styles/goBackButton.module.css'
+
+
+export const GoBackButton = () => {
+    const navigate = useNavigate()
+    const handleNavigate = () => {
+        navigate("/search-results")
+    }
+  return (
+    <>
+    <div className={buttonStyles.button}>
+    <DigiButton
+        afSize={ButtonSize.MEDIUM}
+        afVariation={ButtonVariation.SECONDARY}
+        afFullWidth={false}
+        onAfOnClick={handleNavigate}
+        
+        >
+            Tillbaka
+    <DigiIcon slot='icon' afName="chevron-left">
+           
+        </DigiIcon> 
+  
+    </DigiButton>
+    </div>
+    </>
+  )
+}
