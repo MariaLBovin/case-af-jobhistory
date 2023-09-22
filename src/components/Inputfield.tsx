b30a8d54f57f43acda6cae9658161f9186075985
etrigo1 <bjorn.sjostedt@gmail.com>
2023-09-22 12:30:01 +0200

    Name change on css module for Inputfield

--- src/components/Inputfield.tsx
+++ src/components/Inputfield.tsx
@@ -8,7 +8,7 @@ import {
   DigiFormInput,
   DigiFormValidationMessage,
 } from "@digi/arbetsformedlingen-react";
-import searchStyles from "../styles/search.module.css";
+import inputfieldStyles from "../styles/Inputfield.module.css";
 import { useState } from "react";
 import { DigiFormInputCustomEvent } from "@digi/arbetsformedlingen/dist/types/components";
 import { ISearchProps } from "../models/ISearchProps";
@@ -35,7 +35,7 @@ export const Inputfield = ({
 
   return (
     <>
-      <div className={searchStyles.search_container}>
+      <div className={inputfieldStyles.search_container}>
         {/* <form onSubmit={handleSubmit(onSubmit)}> */}
         <DigiFormInput
           afLabel='Sök efter arbetsgivare med namn eller organisationsnummer'
