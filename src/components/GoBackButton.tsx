b428ae26743ae37b457d59749450b1e50fc2503b
MariaLBovin <maria.bovin@gmail.com>
2023-09-26 10:22:28 +0200

    styles added depending on pathname

--- src/components/GoBackButton.tsx
+++ src/components/GoBackButton.tsx
@@ -1,18 +1,19 @@
-import { useNavigate} from "react-router-dom";
+import { useLocation, useNavigate} from "react-router-dom";
 import buttonStyles from '../styles/GoBackButton.module.css'
 import { ButtonSize, ButtonVariation } from "@digi/arbetsformedlingen"
 import { DigiButton, DigiIcon, } from "@digi/arbetsformedlingen-react"
 
 export const GoBackButton = () => {
   const navigate = useNavigate();
-
+  
+  const location = useLocation()
 
   const handleNavigate = () => {
     navigate(-1)
   };
 
   return (
-    <div className={buttonStyles.button}>
+    <div className={location.pathname === '/contact' ? buttonStyles.contactButton : buttonStyles.button}>
       <DigiButton
         afSize={ButtonSize.MEDIUM}
         afVariation={ButtonVariation.SECONDARY}
