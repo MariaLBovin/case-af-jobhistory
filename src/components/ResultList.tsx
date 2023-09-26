2f32557e644bf8683f139c912a233efb3a6031d9
JoeldelPilar <103323456+JoeldelPilar@users.noreply.github.com>
2023-09-26 15:51:47 +0200

    :sparkle: add spinner when loading ads

--- src/components/ResultList.tsx
+++ src/components/ResultList.tsx
@@ -1,30 +1,35 @@
-import { IAd } from "@/models/IAd";
-import { Link } from "react-router-dom";
-import listStyle from "@/styles/displaySearchReslut.module.css";
+import { IAd } from '@/models/IAd';
+import listStyle from '@/styles/displaySearchReslut.module.css';
+import { Link } from 'react-router-dom';
+import { LoadingSpinner } from './LoadingSpinner';
 
 interface IResultlistProps {
-  filteredAds: IAd[];
+	filteredAds: IAd[];
 }
 
 export const ResultList = ({ filteredAds }: IResultlistProps) => {
-  const formatedDate = (dateString: string): string => {
-    const date = new Date(dateString);
-    return date.toLocaleDateString(undefined);
-  };
+	const formatedDate = (dateString: string): string => {
+		const date = new Date(dateString);
+		return date.toLocaleDateString(undefined);
+	};
 
-  return (
-    <>
-      <ul>
-        {filteredAds.map((ad, index) => (
-          <li key={index} className={listStyle.li}>
-            <Link to={`/ad/${ad.id}`}>
-              <h3>{ad.employer.name}</h3>
-              <p>{ad.occupation.label}</p>
-              <p>Publiceringsdatum {formatedDate(ad.publication_date)}</p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+	if (filteredAds.length === 0) {
+		return <LoadingSpinner />;
+	}
+
+	return (
+		<>
+			<ul>
+				{filteredAds.map((ad, index) => (
+					<li key={index} className={listStyle.li}>
+						<Link to={`/ad/${ad.id}`}>
+							<h3>{ad.employer.name}</h3>
+							<p>{ad.occupation.label}</p>
+							<p>Publiceringsdatum {formatedDate(ad.publication_date)}</p>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
 };
